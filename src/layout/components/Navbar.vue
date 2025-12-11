ac33a7b70be123ed2ae310add1fc46c3286d60c6
shimengdi1211 <13679206892@163.com>
2025-12-11 17:04:14 +0800

    退出删除chat会话的localstorage

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -76,6 +76,7 @@ export default {
     },
     async logout() {
       await this.$store.dispatch('user/logout')
+      localStorage.setItem('ai_chat_history', '')
       this.$router.push(`/login?redirect=${this.$route.fullPath}`)
     }
   }
